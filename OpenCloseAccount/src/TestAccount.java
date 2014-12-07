f6c0b72f22877f0fb88316eac63229412c36fbfd
secant <vivian98.fang@gmail.com>
2014-12-07 13:29:13 -1000

    Chapter 5 Lab

---
+++ OpenCloseAccount/src/TestAccount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class TestAccount 
+{
+	public static void main(String[] args)
+	{
+		Account testAcct;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How many accounts would you like to create?");
+		int num = scan.nextInt();
+		for (int i=1; i<=num; i++)
+		{
+			testAcct = new Account(100, "Name", i);
+			System.out.println("\nCreated account " + testAcct);
+			System.out.println("Now there are " + Account.numAccounts() + " accounts");
+		}
+	}
+}
